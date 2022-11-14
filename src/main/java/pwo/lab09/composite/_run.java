4d618ae11ac07195fdfd73ffdfa70ff28564f2b5
Rafał Cisek <rafal.cisek121@gmail.com>
2022-11-14 17:02:14 +0100

    Zadanie 9.5

--- src/main/java/pwo/lab09/composite/_run.java
+++ src/main/java/pwo/lab09/composite/_run.java
@@ -1,7 +1,15 @@
 package pwo.lab09.composite;
 
 public class _run {
-    public static void main (String[] args) {
-
+    public static void main(String[] args) {
+        WaterSystem waterSystem = new WaterSystem();
+        waterSystem.addComponent(new Tank());
+        waterSystem.addComponent(new Tap());
+        System.out.println("Napełnienie:");
+        waterSystem.fill();
+        waterSystem.afterFill();
+        System.out.println("\nOpróżnienie:");
+        waterSystem.drain();
+        waterSystem.afterDrain();
     }
 }
