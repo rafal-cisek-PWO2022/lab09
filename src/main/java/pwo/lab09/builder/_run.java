ae8767b81c29de173bf77bb707ff5ac1fda15b3d
Rafał Cisek <rafal.cisek121@gmail.com>
2022-11-14 16:04:25 +0100

    zadanie 9.2

--- src/main/java/pwo/lab09/builder/_run.java
+++ src/main/java/pwo/lab09/builder/_run.java
@@ -1,7 +1,16 @@
 package pwo.lab09.builder;
 
 public class _run {
-    public static void main (String[] args) {
-
+    public static void main(String[] args) {
+        TxtPic1 txtPic = new TxtPic1('.', '*', 20, "txtpic1.txt");
+        txtPic.save();
+        TxtPic2 txtPic2 = new TxtPic2();
+        txtPic2
+                .setBackground('_')
+                .setForeground('*')
+                .setSize(20)
+                .setFileName("txtpic2.txt")
+                .createTxtPic()
+                .save();
     }
 }
