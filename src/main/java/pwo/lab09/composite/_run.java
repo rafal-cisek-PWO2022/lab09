40049613e0dcd75a78c902dc4ea5db7908359a5e
Rafał Cisek <rafal.cisek121@gmail.com>
2022-11-14 15:57:07 +0100

    z9.1

---
+++ src/main/java/pwo/lab09/composite/_run.java
@@ -0,0 +1,7 @@
+package pwo.lab09.composite;
+
+public class _run {
+    public static void main (String[] args) {
+
+    }
+}
---
+++ src/test/java/pwo/lab09/AppTest.java
@@ -0,0 +1,20 @@
+package pwo.lab09;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
